6b8cc0647712177d6f8735d2d515e04281934c37
EthanDeLaRosa1 <112013210+EthanDeLaRosa1@users.noreply.github.com>
2024-11-27 01:46:01 -0600

    yes

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 # Me and Zack Smile
 
 from flask import Flask, render_template
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+run_with_ngrok(app)  # This starts ngrok when the app runs
+
 
 # Route for the homepage
 @app.route('/')
