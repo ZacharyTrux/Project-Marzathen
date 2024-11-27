241005fef99c1ba3d42fad0a3df790f69facf14d
EthanDeLaRosa1 <112013210+EthanDeLaRosa1@users.noreply.github.com>
2024-11-27 01:08:41 -0600

    Commit

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-#me and zack Smile
-
-
-# Yip
+# Me and Zack Smile
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Route for the homepage
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# Route for the secret page
+@app.route('/secret')
+def secret():
+    return render_template('secret.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
