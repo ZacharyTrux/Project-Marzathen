830ef26ff82c1edc8f4735908ddaf9f1d1adde08
EthanDeLaRosa1 <112013210+EthanDeLaRosa1@users.noreply.github.com>
2024-11-29 01:02:36 -0600

    ds

--- app.py
+++ app.py
@@ -12,5 +12,34 @@ def home():
 def secret():
     return render_template('developerInfo.html')
 
+@app.route('/get_standings', methods=['GET'])
+def get_standings():
+    team = request.args.get('team')
+    sport = request.args.get('sport')
+
+    # Make API request to ESPN standings endpoint
+    url = f"https://site.api.espn.com/apis/site/v2/sports/{sport}/{sport}/standings"
+    
+    try:
+        response = requests.get(url)
+        data = response.json()
+        
+        # Search for the specific team in the standings data
+        team_data = None
+        for conference in data['children']:
+            for team_info in conference['standings']['entries']:
+                if team_info['team']['displayName'].lower() == team.lower():
+                    team_data = team_info
+                    break
+            if team_data:
+                break
+        
+        if team_data:
+            return jsonify(team_data)
+        else:
+            return jsonify({"error": "Team not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
